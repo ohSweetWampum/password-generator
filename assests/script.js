c2ac747bfdb79202ebaab949cef82c3d14097abc
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-03-28 21:57:11 -0500

    Started by making an array for every possible character

--- assests/script.js
+++ assests/script.js
@@ -1,4 +1,12 @@
-// Assignment code here
+
+// setting all my global variables
+var alphabetUpper = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabetLower = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//Have to add a backslash "\ to the 2 and 24th items of this array, something about escape character?
+var specialCharacters = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+var numericalCharacters = ["0","1","2","3","4","5","6","7","8","9",];
+
+
 
 
 // Get references to the #generate element
@@ -10,7 +18,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  ;
 }
 
 // Add event listener to generate button
