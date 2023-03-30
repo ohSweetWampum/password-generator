120073b3318019254f44a73baf24aab8d73ef89a
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-03-29 20:33:47 -0500

    combined all the lowercase, uppercase, number, and special character arrays into 1 big array

--- assests/script.js
+++ assests/script.js
@@ -5,14 +5,6 @@
 var allCharactersLettersNumbers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~","0","1","2","3","4","5","6","7","8","9"];
 
 
-
-
-// // setting all my global variables
-// var alphabetUpperList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// var alphabetLowerList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var numericalCharactersList = ["0","1","2","3","4","5","6","7","8","9",];
-// //Have to add a backslash "\ to the 2 and 24th items of this array, something about escape character?
-// var specialCharactersList = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 var minLength = 8;
 var maxLength = 128;
 var yes = "yes";
