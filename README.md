44483fdd62e486c1e6aff7d49db5b5f7df736b00
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-03-30 20:54:46 -0500

    Fixed some formatting issues, put intiateEntireProcess() in front of the generateRandomPassword function. put var password and var passwordText into generateRandomPassword function

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # password-generator
-This app generates a random password
+
+A password generator that can generate random passwords based on criteria that the user has selected
+
+## Visit the Site
+
+[Cleck Here to Visit the Deployed Site](https://ohsweetwampum.github.io/password-generator/)
+
+## Description
+
+Code refactoring is the process of restructuring exsisting code to improve it's structure in a way that does not change it's original functionality. Horiseon wanted their code to be more accessible so that people with disabilities can navigate the website, and so the page will rank higher in search engines. Horieson aslo wanted the code consolidated, but did not want the webpage behavior to change. For all these reasons, it was decided that I should refactor it.
+
+## Technology Used
+
+- JavaScript
+  [Learn about Git](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+- HTML
+  [Learn about HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+
+- CSS
+  [Learn about CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+
+- Git
+  [Learn about Git](https://git-scm.com/)
+
+## Usage
+
+1.) Click red "Generate Password".
+2.) Enter number between 8-128.
+3.) Enter "yes" or "no" to having lower case letters included.
+4.) Enter "yes" or "no" to having upper case letters included.
+5.) Enter "yes" or "no" to having special characters included.
+6.) Your password will show up in the text box!
+
+![Alt Text](./assests/gifs/Password-App-Demo.gif)
+
+## Learning Points
+
+## Author Info
+
+Matthew Gibson
+
+- [Portfolio](https://github.com/ohSweetWampum)
+- [LinkedIn](https://www.linkedin.com/in/matthew-gibson-6b9b12237/)
+- [Github](https://github.com/ohSweetWampum)
+
+## Credits
+
+[w3schools.com](https://www.w3schools.com/css/css_selectors.asp)
+
+[w3schools.com](https://www.w3schools.com/html/html5_semantic_elements.asp)
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [ohSweetWampum]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
--- assests/script.js
+++ assests/script.js
@@ -1,37 +1,46 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
+
+//Global arrays
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharactersList = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+
+//Global Varaiables
 var minLength = 8;
 var maxLength = 128;
 var yes = "yes";
 var no = "no";
+
+//Initializing Empty Gloabal Arrays which will be used to store user selections
 var usersCriteriaCharacterArray = [];
 var passwordLength = "";
 
+
+//Function that asks user for desired character length and validates it to ensure it is between 8-128
 function determineLength(){
   var length = prompt("How many characters long do you want your password to be? Number must be between 8-128.");
   if(length >= minLength && length <= maxLength){
     alert("You selected " + length + " characters long.");
     passwordLength = parseInt(length, 10);
-  }else if(length < minLength || length > maxLength){ 
+  } else if(length < minLength || length > maxLength){ 
     alert(" Looks like you chose a number outside the range of 8-128, please select a number inside this range. ");
-      determineLength();
-    }
-    else {
-      alert("You have entered an invalid option, please enter again." );
-      determineLength();
-    }
+    determineLength();
+  } else{
+    alert("You have entered an invalid option, please enter again." );
+    determineLength();
+  }
 }
 
-// asking user if they want lower case letters
+// Function that asks the user if they want lowercase letters in their password, it validates the input to ensure either "yes" or "no" are selected
 function wantLower(){
   var lower = prompt("Do you want lower case letters included in the password, yes or no?");
   if(lower.toLowerCase() === yes){
     alert("You selected " + lower + " to including lower case.");
     usersCriteriaCharacterArray = usersCriteriaCharacterArray.concat(lowerCase);
-  }else if(lower.toLowerCase() === no){
+  } else if(lower.toLowerCase() === no){
     alert("You do not want lower case letters in your password.");
   } else {
     alert("Invalid option , enter yes or no");
@@ -39,13 +48,13 @@ function wantLower(){
   }
 }
  
-// asking user if they want upper case letters
+// Function that asks the user if they want uppercase letters in their password, it validates the input to ensure either "yes" or "no" are selected
 function wantUpper(){
   var upper = prompt("Do you want upper case letters included in the password, yes or no?");
   if(upper.toLowerCase() === yes){
     alert("You selected " + upper + " to including upper case.");
     usersCriteriaCharacterArray = usersCriteriaCharacterArray.concat(upperCase);
-  }else if(upper.toLowerCase() === no){
+  } else if(upper.toLowerCase() === no){
     alert("You do not want upper case in your password.");
   } else {
     alert("Invalid option , enter yes or no");
@@ -53,57 +62,55 @@ function wantUpper(){
   }
 }
 
-// asking user if they want special characters
+// Function that asks the user if they want special characters in their password, it validates the input to ensure either "yes" or "no" are selected
 function wantSpecialCharacters(){
   var specialCharacters = prompt("Do you want special characters included in the password, yes or no?");
   if(specialCharacters.toLowerCase() === yes){
     alert("You selected " + specialCharacters + " to including special characters.");
     usersCriteriaCharacterArray = usersCriteriaCharacterArray.concat(specialCharactersList);
-  }else if(specialCharacters.toLowerCase() === no){
+  } else if(specialCharacters.toLowerCase() === no){
     alert("You do not want any special characters in your password.");
   } else {
     alert("Invalid option , enter yes or no");
     wantSpecialCharacters();
   }
 }
-        
+
+// Function that asks the user if they want numberss in their password, it validates the input to ensure either "yes" or "no" are selected 
 function wantNumbers(){
   var number = prompt("Do you want numbers included in the password, yes or no?");
   if(number.toLowerCase() === yes){
     alert("You selected " + number + " to including numbers.");
     usersCriteriaCharacterArray = usersCriteriaCharacterArray.concat(numbers);
-  }else if(number.toLowerCase() === no){
+  } else if(number.toLowerCase() === no){
     alert("You do not want numbers in your password.");
   } else {
     alert("Invalid option , enter yes or no");
     wantNumbers();
   }
 }
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
+// Function that starts the entire process in correct order
+function intiateEntireProcess(){
+  determineLength();
+  wantLower();
+  wantUpper();
+  wantSpecialCharacters();
+  wantNumbers();
+}
 
-
-// Write password to the #password input
-// this section takes into account what the user selected for the criteria and re-makes the arrays
+//Function that generates a random password based off the user criteria that they chose. Then it sends the value to the HTML element where id= password is in, displaying it on the screen.
 function generateRandomPassword() {
- var password = "";
+  var password = "";
   for(var i = 0; i < passwordLength; i++){
     var randomCharactersGenerator = Math.floor(Math.random() * usersCriteriaCharacterArray.length);
     var selectRandomCharacterFromArray = usersCriteriaCharacterArray[randomCharactersGenerator];
     password += selectRandomCharacterFromArray;
-    }
+    var password = generateRandomPassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
   return password;
-}
-function printOutPassword(){
-determineLength();
-wantLower();
-wantUpper();
-wantSpecialCharacters();
-wantNumbers();
-var password = generateRandomPassword();
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
 }
 
 // Add event listener to generate button
